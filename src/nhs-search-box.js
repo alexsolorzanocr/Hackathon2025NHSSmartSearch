18f4cfd13f5d26f4a619a385b904d14e3c4947b0
Alexander <asolorzano@zondahome.com>
2025-06-02 23:56:04 -0600

    Added a new POC Shell for NHS with responsive support

--- src/nhs-search-box.js
+++ src/nhs-search-box.js
@@ -31,21 +31,12 @@ export class MyElement extends LitElement {
 
   render() {
     return html`
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src=${viteLogo} class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://lit.dev" target="_blank">
-          <img src=${litLogo} class="logo lit" alt="Lit logo" />
-        </a>
-      </div>
       <slot></slot>
       <div class="card">
         <button @click=${this._onClick} part="button">
           count is ${this.count}
         </button>
       </div>
-      <p class="read-the-docs">${this.docsHint}</p>
     `
   }
 
@@ -55,32 +46,10 @@ export class MyElement extends LitElement {
 
   static get styles() {
     return css`
-      :host {
-        max-width: 1280px;
-        margin: 0 auto;
-        padding: 2rem;
-        text-align: center;
-      }
 
-      .logo {
-        height: 6em;
-        padding: 1.5em;
-        will-change: filter;
-        transition: filter 300ms;
-      }
-      .logo:hover {
-        filter: drop-shadow(0 0 2em #646cffaa);
-      }
-      .logo.lit:hover {
-        filter: drop-shadow(0 0 2em #325cffaa);
-      }
 
       .card {
-        padding: 2em;
-      }
-
-      .read-the-docs {
-        color: #888;
+        padding: 0;
       }
 
       a {
@@ -93,8 +62,7 @@ export class MyElement extends LitElement {
       }
 
       ::slotted(h1) {
-        font-size: 3.2em;
-        line-height: 1.1;
+        font-size: 2.2em;
       }
 
       button {
